bd02d1adcfa9e7556e070a62b20def09fff04c91
Henryque <henryque_1@hotmail.com>
2021-01-25 10:59:14 -0300

    Edit, Put and Delete routes added - updated files - 22/01/21

--- routes.js
+++ routes.js
@@ -14,9 +14,15 @@ routes.get('/teachers/create', (req, res) => {
     return res.render('teachers/create');
 })
 
+routes.get('/teachers/:id', teachers.show);
+
+routes.get('/teachers/:id/edit', teachers.edit);
+
 routes.post('/teachers', teachers.post);
 
-routes.get('/teachers/:id', teachers.show);
+routes.put('/teachers', teachers.put);
+
+routes.delete('/teachers', teachers.delete);
 
 
 routes.get('/students', (req, res) => {
--- teachers.js
+++ teachers.js
@@ -3,7 +3,7 @@ const data = require('./data.json');
 const Intl = require('intl');
 const { age, date } = require('./utils.js');
 
-//show
+// show
 exports.show = (req, res) => {
     const { id } = req.params;
 
@@ -26,8 +26,7 @@ exports.show = (req, res) => {
 
 }
 
-
-//create
+// create
 exports.post = (req, res) => {
     const keys = Object.keys(req.body);
 
@@ -39,9 +38,9 @@ exports.post = (req, res) => {
 
     let {avatar_url, name, birth, education_level, class_modality, subjects} = req.body;
     
-    birth = Date.parse(req.body.birth);
+    birth = Date.parse(birth);
     const created_at = Date.now();
-    const id = Number(data.teachers.length + 1);
+    const id = Number (data.teachers.length + 1);
 
     data.teachers.push({
         id,
@@ -61,4 +60,72 @@ exports.post = (req, res) => {
     })
 }
 
+// edit
+exports.edit = (req, res) => {
+    const { id } = req.params;
 
+    const foundTeacher = data.teachers.find((teacher) => {
+        return teacher.id == id;
+    });
+
+    if (!foundTeacher) {
+        return res.send('Teacher not found!');
+    }
+
+    const teacher = {
+        ...foundTeacher,
+        birth: date(foundTeacher.birth)
+    }
+
+    return res.render("teachers/edit", { teacher });
+
+}
+
+// put
+exports.put = (req, res) => {
+    const { id } = req.body;
+    let index = 0;
+
+    const foundTeacher = data.teachers.find((teacher, foundIndex) => {
+        if (id == teacher.id) {
+            index = foundIndex;
+
+            return true;
+        }
+    });
+
+    if (!foundTeacher) {
+        return res.send('Teacher not found!');
+    }
+
+    const teacher = {
+        ...foundTeacher,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.teachers[index] = teacher;
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
+        if (err) return res.send('Write error! -- line 110');
+
+        return res.redirect(`/teachers/${id}`);
+    });
+}
+
+// delete 
+exports.delete = (req, res) => {
+    const { id } = req.body;
+
+    const filteredTeachers = data.teachers.filter(teacher => {
+        return teacher.id != id;
+    });
+
+    data.teachers = filteredTeachers;
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
+        if (err) return res.send('Write error! -- line 127');
+
+        return res.redirect('/teachers');
+    });
+}
