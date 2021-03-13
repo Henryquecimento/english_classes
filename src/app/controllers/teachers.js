5013054f43bb8e7114ad53fa63499d6132a85c5c
Henryque <henryque_1@hotmail.com>
2021-03-13 19:57:06 -0300

    inserting data in the DB -- 13/03/21

--- src/app/controllers/teachers.js
+++ src/app/controllers/teachers.js
@@ -1,5 +1,6 @@
 const Teacher = require("../models/Teacher");
 const { date, grade } = require("../../lib/utils");
+const db = require("../../config/db");
 
 module.exports = {
   index(req, res) {
@@ -9,7 +10,42 @@ module.exports = {
     return res.render("teachers/create");
   },
   post(req, res) {
-    return res.redirect("/teachers");
+
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == 0) {
+        return res.send("Please, fill all the fields!");
+      }
+    }
+
+    const query = `
+      INSERT INTO teachers (
+        avatar_url,
+        name,
+        birth,
+        education_level,
+        class_modality,
+        subjects,
+        created_at
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7)
+      RETURNING id
+    `
+    const values = [
+      req.body.avatar_url,
+      req.body.name,
+      date(req.body.birth).iso,
+      req.body.education_level,
+      req.body.class_modality,
+      req.body.subjects,
+      date(Date.now()).iso
+    ]
+
+    db.query(query, values, (err, results) => {
+      if (err) throw 'Database Error!';
+
+      return res.redirect(`/teachers/${results.rows[0].id}`);
+    })
   },
   show(req, res) {
     return res.render("teachers/show");
---
+++ src/app/models/Teacher.js
@@ -0,0 +1,8 @@
+const db = require('../../config/db');
+const { date, grade } = require("../../lib/utils");
+
+module.exports = {
+    all() {
+
+    }
+}
