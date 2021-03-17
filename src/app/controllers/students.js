77e9eb275d672ebd0aa3895fd62e7771ca3823c4
Henryque <henryque_1@hotmail.com>
2021-03-17 09:47:32 -0300

    Students model and controller created/adjusted -- 17/03/21

--- src/app/controllers/students.js
+++ src/app/controllers/students.js
@@ -1,26 +1,68 @@
 const Student = require("../models/Student");
-const { date, grade } = require("../../lib/utils");
+const { grade, date } = require("../../lib/utils");
 
 module.exports = {
   index(req, res) {
-    return res.render("./students/student");
+    Student.all((students) => {
+      for (student of students) {
+        student.education_level = grade(student.education_level);
+      }
+
+      return res.render("students/student", { students });
+    });
   },
   create(req, res) {
     return res.render("students/create");
   },
   post(req, res) {
-    return res.redirect("/students");
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == 0) {
+        return res.send("Please, fill all the fields!");
+      }
+    }
+
+    Student.create(req.body, (student) => {
+      return res.redirect(`/students/${student.id}`);
+    });
   },
   show(req, res) {
-    return res.render("students/show");
+    Student.find(req.params.id, (student) => {
+      if (!student) return res.send("Student not found!");
+
+      student.birth = date(student.birth).birthDay;
+      student.education_level = grade(student.education_level);
+      student.created_at = date(student.created_at).format;
+
+      return res.render("students/show", { student });
+    });
   },
   edit(req, res) {
-    return res.render("students/edit");
+    Student.find(req.params.id, (student) => {
+      if (!student) return res.send("Student not found!");
+
+      student.birth = date(student.birth).iso;
+
+      return res.render("students/edit", { student });
+    });
   },
   put(req, res) {
-    return res.redirect(`/students`);
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == 0) {
+        return res.send("Please, fill all the fields!");
+      }
+    }
+
+    Student.update(req.body, () => {
+      return res.redirect(`/students/${req.body.id}`);
+    });
   },
   delete(req, res) {
-    return res.redirect("/students");
+    Student.delete(req.body.id, () => {
+      return res.redirect("/students");
+    });
   },
 };
--- src/app/models/Student.js
+++ src/app/models/Student.js
@@ -1,2 +1,94 @@
 const db = require("../../config/db");
 const { date, grade } = require("../../lib/utils");
+
+module.exports = {
+  all(callback) {
+    db.query(
+      `
+        SELECT * FROM students
+      `,
+      (err, results) => {
+        if (err) throw `Database Error!${err}`;
+
+        callback(results.rows);
+      }
+    );
+  },
+  create(data, callback) {
+    const query = `
+      INSERT INTO students (
+        avatar_url,
+        name,
+        email,
+        birth,
+        education_level,
+        workload,
+        created_at
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7)
+      RETURNING id
+    `;
+
+    const values = [
+      data.avatar_url,
+      data.name,
+      data.email,
+      date(data.birth).iso,
+      data.education_level,
+      data.workload,
+      date(Date.now()).iso,
+    ];
+
+    db.query(query, values, (err, results) => {
+      if (err) throw `Database Error!${err}`;
+
+      callback(results.rows[0]);
+    });
+  },
+  find(id, callback) {
+    db.query(` SELECT * FROM students WHERE id = $1`, [id], (err, results) => {
+      if (err) throw `Database Error! ${err}`;
+
+      callback(results.rows[0]);
+    });
+  },
+  update(data, callback) {
+    const query = `
+    UPDATE students SET
+      avatar_url=($1),
+      name=($2),
+      email=($3),
+      birth=($4),
+      education_level=($5),
+      workload=($6)
+    WHERE id = $7
+    `;
+
+    const values = [
+      data.avatar_url,
+      data.name,
+      data.email,
+      date(data.birth).iso,
+      data.education_level,
+      data.workload,
+      data.id,
+    ];
+
+    db.query(query, values, (err, results) => {
+      if (err) throw `Database Error! ${err}`;
+
+      callback();
+    });
+  },
+  delete(id, callback) {
+    db.query(
+      `
+      DELETE FROM students WHERE id = $1`,
+      [id],
+      (err, results) => {
+        if (err) throw `Database Error! ${err}`;
+
+        return callback();
+      }
+    );
+  },
+};
