369fab61afc261dfe454c482bde4ad6b6d138f77
Henryque <henryque_1@hotmail.com>
2021-03-24 17:40:22 -0300

    Paginate logic (incomplete) added in  scripts.js -- 24/03/21

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,39 @@
 const currentPage = location.pathname;
-let menuItems = document.querySelectorAll('header .links a');
+let menuItems = document.querySelectorAll("header .links a");
 
 for (item of menuItems) {
-    if (currentPage.includes(item.getAttribute('href'))) {
-        item.classList.add('active');
-    };
-};
+  if (currentPage.includes(item.getAttribute("href"))) {
+    item.classList.add("active");
+  }
+}
+
+/* Pagination */
+
+// [1,..., 13, 14, 15, 16, 17, ..., 20];
+
+function paginate(selectedPage, totalPage) {
+  let pages = [],
+    oldPage;
+
+  for (let currentPage = 1; currentPage <= totalPage; currentPage++) {
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPage;
+    const afterSelectedPage = currentPage <= selectedPage + 2;
+    const beforeSelectedPage = currentPage >= selectedPage - 2;
+
+    if (firstAndLastPage || (beforeSelectedPage && afterSelectedPage)) {
+      if (oldPage && currentPage - oldPage > 2) {
+        pages.push("...");
+      }
+
+      if (oldPage && currentPage - oldPage == 2) {
+        pages.push(currentPage - 1);
+      }
+
+      pages.push(currentPage);
+
+      oldPage = currentPage;
+    }
+  }
+
+  /*   console.log(pages); */
+}
